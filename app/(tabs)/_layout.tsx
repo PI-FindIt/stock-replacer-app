e82d55536a0c932724e53da382d39c1f86b24709
falcaodiogo <falcao.diogo@ua.pt>
2025-04-24 14:16:18 +0100

    Adds Warnings tab with icon and screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,10 @@
 import { Tabs } from "expo-router";
-import { Home, NotebookPen, Route } from "lucide-react-native";
+import {
+  Home,
+  MessageCircleWarning,
+  NotebookPen,
+  Route,
+} from "lucide-react-native";
 import { HapticTab } from "@/components/HapticTab";
 import React from "react";
 import TabBar from "@/components/TabBar";
@@ -7,6 +12,9 @@ import TabBar from "@/components/TabBar";
 const HomeIcon = ({ color }: { color: string }) => <Home color={color} />;
 const NotebookPenIcon = ({ color }: { color: string }) => (
   <NotebookPen color={color} />
+);
+const WarningIcon = ({ color }: { color: string }) => (
+  <MessageCircleWarning color={color} />
 );
 const RouteIcon = ({ color }: { color: string }) => <Route color={color} />;
 
@@ -25,6 +33,13 @@ export default function TabLayout() {
         options={{
           title: "Home",
           tabBarIcon: HomeIcon,
+        }}
+      />
+      <Tabs.Screen
+        name="warnings"
+        options={{
+          title: "Warnings",
+          tabBarIcon: WarningIcon,
         }}
       />
       <Tabs.Screen
